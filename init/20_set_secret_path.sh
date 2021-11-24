824536004fc1f6483e8a03fe7091a052e6712232
Robert Loomans <robert@loomans.org>
2021-11-24 17:52:09 +1000

    minor fixes

--- init/20_set_secret_path.sh
+++ init/20_set_secret_path.sh
@@ -4,7 +4,7 @@ set -o nounset
 
 new_secret=$(perl -e '@chars = (0..9, q(a)..q(z), q(A)..q(Z), q(_)); print join q(), map $chars[rand @chars], 0..12;')
 
-if [ ! -e /opt/etc/cgiproxy.conf -a ! -e /opt/cgiproxy/etc/apache-cgiproxy.conf ]; then
+if [ ! -e /opt/etc/cgiproxy.conf -o ! -e /opt/cgiproxy/etc/apache-cgiproxy.conf ]; then
   perl -p -E 's{^\$SECRET_PATH=.*;\$}{\$SECRET_PATH= "'"$new_secret"'" ;}' /opt/cgiproxy/etc/cgiproxy.conf.template > /opt/cgiproxy/etc/cgiproxy.conf
   chown root:www-data /opt/cgiproxy/etc/cgiproxy.conf
   chmod 0640 /opt/cgiproxy/etc/cgiproxy.conf
